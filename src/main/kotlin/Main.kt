746b7789a856f9f48803e29dcfd49a9996560cd8
mawinda <mawindavic@gmail.com>
2023-08-10 20:09:13 +0300

    prints infor only after desired results are found

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,10 +8,23 @@ const val pdValue: Double = 15.8
 const val lgdValue: Double = 45.0
 
 const val rateChange: Double = 0.01
+
+
+data class RatingRow(
+    val month: Int,
+    val opening: Double,
+    val interest: Int,
+    val payment: Int,
+    val closing: Double,
+    val cof: Double,
+    val coc: Double,
+    val cr: Double
+)
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
- val rate = riskRatePricingRate(rate = baseRateValue, term = 12, principal = 100000.0)
+    val rate = riskRatePricingRate(rate = baseRateValue, term = 12, principal = 100000.0)
     println("Rate: $rate")
 
 }
@@ -19,20 +32,33 @@ fun main(args: Array<String>) {
 private fun riskRatePricingRate(rate: Double, term: Int, principal: Double): Double {
     val payment = monthlyPayment(rate = rate, term = term, principal = principal).roundToInt()
     val interests: MutableList<Int> = mutableListOf()
+    val rows: MutableList<RatingRow> = mutableListOf()
     var mBalance = principal
-
     for (i in 1..term) {
+        val openingBalance = mBalance
         val cof = costOfFund(baseRate = baseRateValue, mBalance)
         val coc = costOfCapital(roe = roEValue, balance = mBalance)
         val cr = creditRisk(pd = pdValue, lgd = lgdValue, balance = mBalance)
         val interest = (cof + coc + cr).roundToInt()
+        interests.add(interest)
         mBalance = mBalance - (payment - interest)
-        interests.add(interest).also {
-            println("Month: $i Closing balance: $mBalance  Repayment: $payment  Interest: $interest")
-        }
+        rows.add(
+            RatingRow(
+                month = i,
+                opening = openingBalance,
+                interest = interest,
+                payment = payment,
+                closing = mBalance,
+                cof = cof,
+                coc = coc,
+                cr = cr
+            )
+        )
     }
 
-    return if (mBalance < 0) rate else riskRatePricingRate(
+    return if (mBalance < 0) rate.also {
+        println(rows.joinToString("\n"))
+    } else riskRatePricingRate(
         rate = rate.plus(rateChange),
         term = term,
         principal = principal
