9e1b1e39d207fabf969782d6541406cb856b081e
Mawinda <mawindavic@gmail.com>
2023-08-11 10:01:56 +0300

    adds group calculations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,13 +4,11 @@ import kotlin.math.roundToInt
 
 const val baseRateValue: Double = 13.7
 const val roEValue: Double = 2.0
-const val pdValue: Double = 15.8
 const val lgdValue: Double = 45.0
 
 const val rateChange: Double = 0.01
 
-
-data class RatingRow(
+data class Row(
     val month: Int,
     val opening: Double,
     val interest: Int,
@@ -18,32 +16,39 @@ data class RatingRow(
     val closing: Double,
     val cof: Double,
     val coc: Double,
-    val cr: Double
+    val cr: Double,
 )
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    val rate = riskPricingRate(rate = baseRateValue, term = 12, principal = 100000.0)
+    val pdValue = pdValueCalc(group = 60)
+    val rate = riskPricingRate(rate = baseRateValue, term = 12, principal = 100000.0, pdValue = pdValue)
     println("Rate: $rate")
-
 }
 
-private fun riskPricingRate(rate: Double, term: Int, principal: Double): Double {
+private fun pdValueCalc(group: Int): Double {
+    return when (group) {
+        in 1..60 -> 15.75
+        in 61..80 -> 8.75
+        else -> 5.5
+    }
+}
+
+private fun riskPricingRate(rate: Double, term: Int, principal: Double, pdValue: Double): Double {
+    // calculates monthly payment using pmt
     val payment = monthlyPayment(rate = rate, term = term, principal = principal).roundToInt()
-  //  val interests: MutableList<Int> = mutableListOf()
-    val rows: MutableList<RatingRow> = mutableListOf()
+
+    // Harmonize with the monthly repayment found above
+    val rows: MutableList<Row> = mutableListOf()
     var mBalance = principal
     for (i in 1..term) {
         val openingBalance = mBalance
-        val cof = costOfFund(baseRate = baseRateValue, mBalance)
-        val coc = costOfCapital(roe = roEValue, balance = mBalance)
-        val cr = creditRisk(pd = pdValue, lgd = lgdValue, balance = mBalance)
+        val cof = costOfFund(baseRate = baseRateValue, balance = openingBalance)
+        val coc = costOfCapital(roe = roEValue, balance = openingBalance)
+        val cr = creditRisk(pd = pdValue, lgd = lgdValue, balance = openingBalance)
         val interest = (cof + coc + cr).roundToInt()
-      //  interests.add(interest)
-        mBalance = mBalance - (payment - interest)
+        mBalance -= (payment - interest)
         rows.add(
-            RatingRow(
+            Row(
                 month = i,
                 opening = openingBalance,
                 interest = interest,
@@ -51,29 +56,33 @@ private fun riskPricingRate(rate: Double, term: Int, principal: Double): Double 
                 closing = mBalance,
                 cof = cof,
                 coc = coc,
-                cr = cr
-            )
+                cr = cr,
+            ),
         )
     }
 
-    return if (mBalance < 0) rate.also {
-        println(rows.joinToString("\n"))
-    } else riskPricingRate(
-        rate = rate.plus(rateChange),
-        term = term,
-        principal = principal
-    )
+    // If last closing balance is less than 0 print schedule
+    return if (mBalance < 0) {
+        rate.also {
+            println(rows.joinToString("\n"))
+        }
+    } else {
+        riskPricingRate(
+            rate = rate.plus(rateChange),
+            term = term,
+            principal = principal,
+            pdValue = pdValue,
+        )
+    }
 }
 
 private fun monthlyPayment(rate: Double, term: Int, principal: Double): Double =
     abs(Finance.pmt(rate.div(12 * 100), term, principal))
-
 
 private fun costOfFund(baseRate: Double, balance: Double): Double = baseRate.div(12 * 100).times(balance)
 private fun costOfCapital(roe: Double, balance: Double): Double = roe.div(12 * 100).times(balance)
 private fun creditRisk(pd: Double, lgd: Double, balance: Double): Double {
     val mPd = pd.div(100)
     val mlgd = lgd.div(100)
-    val creditRisk = balance * ((mPd * mlgd) / 12)
-    return creditRisk
+    return balance * ((mPd * mlgd) / 12)
 }
